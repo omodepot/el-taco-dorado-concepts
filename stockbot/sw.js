050402618bf447db589d4c9ceb64b8a8ea1dced8
omodepot <support@omnimediaonline.com>
2026-09-19 07:48:19 -0400

    Add StockBot service worker

---
+++ stockbot/sw.js
@@ -0,0 +1,1 @@
+const C='stockbot-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>{if(e.request.method==='GET'&&new URL(e.request.url).origin===location.origin)e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
